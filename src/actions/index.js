318ebc7fb1a24a4bd3b625f8a366ece366e573ea
Elena <katayeva08@gmail.com>
2023-10-20 13:35:59 +0300

    add new repo

---
+++ src/actions/index.js
@@ -0,0 +1,93 @@
+
+
+// export const fetchFilters = (request) => (dispatch) => {
+//   dispatch(filtersFetching());
+//   request("http://localhost:3001/heroes")
+//     .then(data => dispatch(filtersFetched(data)))
+//     .catch(() => dispatch(filtersFetchingError()))
+// }
+// // export const heroesFetching = createAction('HEROES_FETCHING');
+
+// // export const heroesFetching = () => {
+// //   return {
+// //     type: 'HEROES_FETCHING'
+// //   }
+// // }
+
+// // export const heroesFetched = createAction('HEROES_FETCHED');
+
+// // export const heroesFetched = (heroes) => {
+// //   return {
+// //     type: 'HEROES_FETCHED',
+// //     payload: heroes
+// //   }
+// // }
+
+
+// // export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
+
+// // export const heroesFetchingError = () => {
+// //   return {
+// //     type: 'HEROES_FETCHING_ERROR'
+// //   }
+// // }
+
+
+
+// export const filtersFetching = () => {
+//   return {
+//     type: 'FILTERS_FETCHING'
+//   }
+// }
+
+
+// export const filtersFetched = (filters) => {
+//   return {
+//     type: 'FILTERS_FETCHED',
+//     payload: filters
+//   }
+// }
+
+// export const filtersFetchingError = () => {
+//   return {
+//     type: 'FILTERS_FETCHING_ERROR'
+//   }
+// }
+
+// export const activeFilterChanged = (filter) => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch({
+//       type: 'ACTIVE_FILTER_CHANGED',
+//       payload: filter
+
+//     })
+//   }, 1000)
+// }
+
+
+
+// export const activeFilterChanged = (filter) =>{
+//   return {
+//     type: 'ACTIVE_FILTER_CHANGED',
+//     payload: filter
+//   }
+// }
+
+// export const heroCreated = createAction('HERO_CREATED');
+
+// export const heroCreated = (hero) => {
+//   return {
+//     type: 'HERO_CREATED',
+//     payload: hero
+//   }
+// }
+
+
+// export const heroDeleted = createAction('HERO_DELETED');
+
+// export const heroDeleted = (id) => {
+//   return {
+//     type: 'HERO_DELETED',
+//     payload: id
+//   }
+// }
---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,70 @@
+import { useHttp } from '../../hooks/http.hook';
+import { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { heroDeleted, fetchHeroes, filteredHeroesSelector } from './heroesSlice';
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+
+import './heroesList.scss';
+
+// Задача для этого компонента:
+// При клике на "крестик" идет удаление персонажа из общего состояния
+// Усложненная задача:
+// Удаление идет и с json файла при помощи метода DELETE
+
+const HeroesList = () => {
+
+  const filteredHeroes = useSelector(filteredHeroesSelector);
+  const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
+  const dispatch = useDispatch();
+  const { request } = useHttp();
+
+  useEffect(() => {
+    dispatch(fetchHeroes());
+    
+    // eslint-disable-next-line
+  }, []);
+
+  // Функция берет id и по нему удаляет ненужного персонажа из store
+  // ТОЛЬКО если запрос на удаление прошел успешно
+  // Отслеживайте цепочку действий actions => reducers
+  const onDelete = useCallback(
+    (id) => {
+    // Удаление персонажа по его id
+    request(`http://localhost:3001/heroes/${id}`, "DELETE")
+      .then(data => console.log(data, 'Deleted'))
+      .then(dispatch(heroDeleted(id)))
+      .catch(err => console.log(err));
+    // eslint-disable-next-line  
+  }, [request]);
+
+  if (heroesLoadingStatus === "loading") {
+    return <Spinner />;
+  } else if (heroesLoadingStatus === "error") {
+    return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+  }
+
+  const renderHeroesList = (arr) => {
+    if (arr.length === 0) {
+      return (
+          <h5 className="text-center mt-5">Героев пока нет</h5>
+      );
+    }
+
+    return arr.map(({ id, ...props }) => {
+      return (
+          <HeroesListItem  {...props} onDelete={() => onDelete(id)} />
+      );
+    });
+  };
+
+   const elements = renderHeroesList(filteredHeroes);
+    return (
+      <ul>
+        {elements}
+      </ul>
+    );
+};
+
+export default HeroesList;
